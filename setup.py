9df91722d8e8e6c98b78e93831a2e06c18d941f6
Tim Weidner <timaa2k@gmail.com>
2019-10-06 02:04:04 +0200

    Add basic setup.py requirements handling

--- setup.py
+++ setup.py
@@ -1,7 +1,27 @@
+import pathlib
 import setuptools
+from typing import List
 
-with open("README.rst", "r") as fh:
-    long_description = fh.read()
+
+def _get_dependencies(requirements_file: pathlib.Path) -> List[str]:
+    """
+    Return requirements from a requirements file.
+    This expects a requirements file with no ``--find-links`` lines.
+    """
+    lines = requirements_file.read_text().strip().split('\n')
+    print(lines)
+    return [line for line in lines if not line.startswith('#')]
+
+
+INSTALL_REQUIRES = _get_dependencies(
+    requirements_file=pathlib.Path('requirements.txt'),
+)
+
+DEV_REQUIRES = _get_dependencies(
+    requirements_file=pathlib.Path('dev-requirements.txt'),
+)
+
+LONG_DESCRIPTION = pathlib.Path('README.rst').read_text()
 
 setuptools.setup(
     name="priolib",
@@ -9,13 +29,21 @@ setuptools.setup(
     author="Tim Weidner",
     author_email="timaa2k@gmail.com",
     description="TaskPrio client library",
-    long_description=long_description,
+    long_description=LONG_DESCRIPTION,
     long_description_content_type="text/x-rst",
     url="https://github.com/timaa2k/priolib",
-    packages=setuptools.find_packages(),
+    include_package_data=True,
+    packages=setuptools.find_packages(where='src'),
+    package_dir={'': 'src'},
+    zip_safe=False,
+    install_requires=INSTALL_REQUIRES,
+    extras_require={
+        'dev': DEV_REQUIRES,
+    },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GPLv3 License",
         "Operating System :: OS Independent",
     ],
+    dependency_links=[],
 )
